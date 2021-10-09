151fb86ce4c8a28f2b16b527a15f3a0f7e439521
Pr1ncee <andrewvs0707@gmail.com>
2021-10-09 17:23:01 +0300

    Initial Commit

--- queue_bot.py
+++ queue_bot.py
@@ -1,3 +1,5 @@
+import pickle
+
 import telebot
 from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup,\
                           ReplyKeyboardMarkup, KeyboardButton
@@ -11,29 +13,92 @@ data_base = {}
 queue_num = 0
 
 
-def subgroup(message, subgroup_num, first_name, last_name):
+def db_writer(username, subgroup_num=None,
+              queue_id=None, queue_is_created=False,
+              flag=None, db_filename='queueBot_db.pickle'):
+    with open(db_filename, 'rb') as db:
+        try:
+            data = pickle.load(db)
+        except EOFError:
+            data = {}
+
+    with open(db_filename, 'wb') as db_:
+        if username not in data:
+            data.update({username: {'subgroup': subgroup_num, 'queue_id': queue_id,
+                                    'queue_is_created': queue_is_created}})
+            pickle.dump(data, db_)
+        elif subgroup_num:
+            data[username].update({'subgroup': subgroup_num})
+        elif queue_id:
+            data[username].update({'queue_id': queue_id, 'queue_is_created': queue_is_created})
+        elif flag == 'close':
+            data[username].update({'queue_is_created': queue_is_created})
+
+        pickle.dump(data, db_)
+
+
+def db_reader(db_filename='queueBot_db.pickle'):
+    with open(db_filename, 'rb') as db:
+        user_data = pickle.load(db)
+
+    return user_data
+
+
+def subgroup(message, subgroup_num):
     global data_base
-    data_base[' '.join([first_name, last_name])] = {'subgroup': subgroup_num, 'queue_id': None}
+
+    first_name = message.from_user.first_name
+    last_name = message.from_user.last_name
+    username = ' '.join([first_name, last_name])
+
+    db_writer(username, subgroup_num)
+    with open('queueBot_db.pickle', 'rb') as db:
+        data = pickle.load(db)
+        print(data[username])
     bot.send_message(message.chat.id, f"Your subgroup is changed to {subgroup_num}")
 
 
 def queue_create(message):
     global queue_num
-    queue_num += 1
-    bot.send_message(message.chat.id, f"Queue №{queue_num}", reply_markup=inline_keyboard())
+
+    first_name = message.from_user.first_name
+    last_name = message.from_user.last_name
+    username = ' '.join([first_name, last_name])
+
+    user_data = db_reader()
+
+    try:
+        user_data[username]['subgroup']
+    except KeyError:
+        bot.send_message(message.chat.id, "Choose your subgroup at first!")
+    else:
+        queue_num += 1
+
+        if not user_data[username]['queue_is_created']:
+            db_writer(username, queue_id=queue_num, queue_is_created=True)
+            bot.send_message(message.chat.id, f"Queue №{queue_num}", reply_markup=inline_keyboard())
+        else:
+            bot.send_message(message.chat.id, "You have already created the queue!")
 
 
 def queue_close(call):
+    first_name = call.from_user.first_name
+    last_name = call.from_user.last_name
+    username = ' '.join([first_name, last_name])
+
+    db_writer(username, flag='close')
+
     bot.edit_message_text("The queue was deleted", call.message.chat.id, call.message.id)
 
 
 def reply_keyboard():
     reply_kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
+
     btn_make_queue = KeyboardButton('Create a queue')
     btn_subgroup_1 = KeyboardButton('Subgroup 1')
     btn_subgroup_2 = KeyboardButton('Subgroup 2')
 
-    reply_kb.add(btn_make_queue).row(btn_subgroup_1, btn_subgroup_2)
+    reply_kb.row(btn_subgroup_1, btn_subgroup_2).add(btn_make_queue)
     return reply_kb
 
 
@@ -48,21 +113,19 @@ def inline_keyboard():
     return inline_kb
 
 
-@bot.message_handler(content_types=['text'])
-def main(message):
-    first_name = message.from_user.first_name
-    last_name = message.from_user.last_name
-    if message.text == "Subgroup 1":
-        subgroup(message, 1, first_name, last_name)
-    elif message.text == "Subgroup 2":
-        subgroup(message, 2, first_name, last_name)
-    elif message.text == "Create a queue":
-        queue_create(message)
-
-
 @bot.message_handler(commands=['start'])
 def start_command(message):
-    bot.send_message(message.chat.id, "Choose the option you want", reply_markup=reply_keyboard())
+    bot.send_message(message.chat.id, "Choose the subgroup at first: ", reply_markup=reply_keyboard())
+
+
+@bot.message_handler(content_types=['text'])
+def main(message):
+    if message.text == "Subgroup 1":
+        subgroup(message, 1)
+    elif message.text == "Subgroup 2":
+        subgroup(message, 2)
+    elif message.text == "Create a queue":
+        queue_create(message)
 
 
 @bot.callback_query_handler(func=lambda call: True)
